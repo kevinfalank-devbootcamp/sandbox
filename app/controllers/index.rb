36c4f0fd0dd2aea391f801f2cfba65a9075d52fd
Apprentice <apprentice@dbc-chicago-83.local>
2014-01-28 13:28:14 -0600

    sandbox

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,20 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @fred = params[:fred]
+  @post = params[:post]
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @wilma = params[:wilma]
+  erb :post_cool_url
 end
